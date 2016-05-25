0b42009c0e0c8741bc007661eff188cbf89b67a8
raphael <raphael@10.131.128.58>
2016-05-25 13:43:57 +0200

    phase 3.1

--- src/main.java
+++ src/main.java
@@ -3,6 +3,12 @@ public class main {
 
 	public static void main(String[] args) {
 
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel,
+		dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
+		dogfightController.play(); 
 
 	}
 

4ab49c23a1f2a7e67e91d51bc2da29cc7eedca83
raphael <raphael@10.131.128.58>
2016-05-25 15:28:26 +0200

    phase  fini

--- src/jpu2016/dogfight/controller/DogfightController.java
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -3,7 +3,7 @@ package jpu2016.dogfight.controller;
 import jpu2016.dogfight.model.IDogfightModel;
 import jpu2016.dogfight.view.IViewSystem;
 
-public class DogfightController {
+public class DogfightController implements IOrderPerformer {
 
 	static private int TIME_SLEEP = 30;
 	public IDogfightModel dogfightModel ;
